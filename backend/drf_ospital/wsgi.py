c8a394da4bf71256cdd29de7237a41ed67da8520
Mihai <mihail_danut24@yahoo.com>
2024-03-09 12:34:03 +0200

    setted up drf

--- backend/drf_ospital/wsgi.py
+++ backend/drf_ospital/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for cfehome project.
+WSGI config for drf_ospital project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_ospital.settings')
 
 application = get_wsgi_application()
